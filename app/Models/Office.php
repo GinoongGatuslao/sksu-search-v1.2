6b03c07b3395a32dc2f924b6ea921cc2b0bf02b1
GabJeungSook <67619600+GabJeungSook@users.noreply.github.com>
2022-09-07 12:39:07 +0800

    relationshits

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -8,4 +8,28 @@ use Illuminate\Database\Eloquent\Model;
 class Office extends Model
 {
     use HasFactory;
+
+    public function employee_information(){
+        return $this->hasOne('App\Models\Employee_information');
+    }
+
+    public function campus(){
+        return $this->belongsTo('App\Models\Campus');
+    }
+
+    public function head(){
+        return $this->hasOne('App\Models\Employee_information','id','head_id');
+    }
+
+    public function oic_1(){
+        return $this->hasOne('App\Models\Employee_information','id','oic_1');
+    }
+
+    public function oic_2(){
+        return $this->hasOne('App\Models\Employee_information','id','oic_2');
+    }
+
+    public function oic_3(){
+        return $this->hasOne('App\Models\Employee_information','id','oic_3');
+    }
 }
